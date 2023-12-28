d305204cac7500eb4f8c0919969fcde55ab9c9d0
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-28 01:21:18 +0100

    update task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# create a file in /tmp
+# create a file in /tmp Using Puppet.
 
 file { '/tmp/school':
-  content =>'I love Puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
